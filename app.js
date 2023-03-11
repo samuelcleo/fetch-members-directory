bc47d18953e56fc7d820618e70ba60fd2e88ebcd
samuelcleo <cleophassamuel@gmail.com>
2023-03-11 14:38:44 +1100

    Supplied Properties to Modal onclick
    - In a displayModal() function define each relevant property
    - Destructure the relevant member object at index supplied by the matching data-index of event.target .card element.
    - Display the variables from destructured properties by interpolating into modal HTML

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
 const urlAPI = `https://randomuser.me/api/?results=12&inc=name, picture,
 email, location, phone, dob &noinfo &nat=US`;
+
+// Recieves Fetched response and stores in variable: members.
+// Populates display with HTML cards
 let members = [];
-
-const gridContainer = document.querySelector('.grid-container');
-
-const overlay = document.querySelector('.overlay');
-const modalContent = document.querySelector('.modal-content');
-const modalBtnClose = document.querySelector('.modal-close');
-
-function displayModal(index) {
-    let { 
-        name, dob, phone, email, 
-        location: { city, street, state, postcode }, 
-        picture 
-    } = members[index];
-    let date = new Date(dob.date);
-    const modalHTML = `
-        <img class="avatar" src="${picture.large}" />
-        <div class="text-container">
-            <h2 class="name">${name.first} ${name.last}</h2>
-            <p class="email">${email}</p>
-            <p class="address">${city}</p>
-            <hr />
-            <p>${phone}</p>
-            <p class="address">${street}, ${state} ${postcode}</p>
-            <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
-        </div>
-    `;
-    overlay.classList.remove("hidden");
-    modalContainer.innerHTML = modalHTML;
-}
 function displayMembers(membersData) {
     members = membersData;
+    console.log(members);
     let memberHTML = "";
     members.forEach((member, index) => {
         const name = member.name;
@@ -50,6 +25,53 @@ function displayMembers(membersData) {
     })
     gridContainer.innerHTML = memberHTML;
 }
+// Requires variable: members to destructure and display modal with additional properties
+function displayModal(index) {
+    let { 
+            name, 
+            dob, 
+            phone, 
+            email, 
+            location: { city, street, state, postcode }, 
+            picture
+        } = members[index]; // desctructures and declares properties of [ ...members ] as variables for all properties supplied at the object members[index]
+    let date = new Date(dob.date);
+    const modalHTML = `
+    <div class="modal-content">
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr />
+            <p>${phone}</p>
+            <p class="address">${street}, ${state} ${postcode}</p>
+            <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+    </div>
+    `;
+    overlay.classList.remove("hidden");
+    modalContent.innerHTML = modalHTML;
+}
+
+// Lower order operations
+const gridContainer = document.querySelector('.grid-container');
+const overlay = document.querySelector('.overlay');
+const modalContent = document.querySelector('.modal-content');
+
+// supply the matching index in members array from the cards' data-index using the click event
+// run displayModal() using the matched index
+gridContainer.addEventListener('click', (e) => {
+    if (e.target.className === "card") {
+        const index = e.target.getAttribute("data-index");
+        displayModal(index);
+    }
+})
+
+const modalBtnClose = document.querySelector('.modal-close');
+modalBtnClose.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+})
 
 fetch(urlAPI)
     .then(response => response.json())
